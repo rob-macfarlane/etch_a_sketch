521f73c390d98cff4a176e42bd4ef4fa3bbf34cf
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-24 22:29:06 -0800

    Update so that sketching now works

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,9 @@
 
 const sketchBox = document.querySelector('.sketch-box');
 
-numberOfBoxes = 1000;
+numberOfBoxes = 5000;
 
-boxSideLength = 400 / Math.sqrt(numberOfBoxes);
+boxSideLength = 600 / Math.sqrt(numberOfBoxes);
 lengthString = boxSideLength + 'px'
 console.log(lengthString)
 
@@ -19,23 +19,35 @@ for (let i = 0; i < numberOfBoxes; i++){
 
 var drawState = false;
 window.addEventListener("mousedown", event => {
+    //this prevent default line could become troubling if we add a drag function later
+    // however it is used to eliminate erroneous sketching related to sketchbox mouseenter
+    // comment
+    event.preventDefault();
     if(event.button===0){
-        drawstate = true;
+        drawState = true;
     }
 });
-
-var drawState = false;
 window.addEventListener("mouseup", event => {
     if(event.button===0){
-        drawstate = false;
+        drawState = false;
     }
 });
 
 
+// purpose of sketchbox mouse enter event listener
+// It was found that you can get stuck in a drag scenario outside of the drawing square
+// so if that happens this changes the draw state back to false when the mouse enters the sketch area
+// however it doesn't happen quite fast enough so maybe one square will get filled
+sketchBox.addEventListener('mouseenter', event => {
+    drawState = false;
+});
 
 document.querySelectorAll('.sketch-element').forEach(item => {
+    item.addEventListener('mousedown', e => {
+        e.preventDefault();
+    });
     item.addEventListener('mouseover', event => {
-        if (drawstate) {
+        if (drawState) {
             item.style.backgroundColor = 'black';
         }
     });
