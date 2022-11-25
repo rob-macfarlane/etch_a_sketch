344e76d4c352e6109f1dad35b14b313cb80287c7
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-24 21:52:26 -0800

    Sketching function complete

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 
 const sketchBox = document.querySelector('.sketch-box');
 
-numberOfBoxes = 100;
+numberOfBoxes = 1000;
 
 boxSideLength = 400 / Math.sqrt(numberOfBoxes);
 lengthString = boxSideLength + 'px'
@@ -17,8 +17,26 @@ for (let i = 0; i < numberOfBoxes; i++){
     sketchBox.appendChild(sketchElement);
 }
 
+var drawState = false;
+window.addEventListener("mousedown", event => {
+    if(event.button===0){
+        drawstate = true;
+    }
+});
+
+var drawState = false;
+window.addEventListener("mouseup", event => {
+    if(event.button===0){
+        drawstate = false;
+    }
+});
+
+
+
 document.querySelectorAll('.sketch-element').forEach(item => {
     item.addEventListener('mouseover', event => {
-        item.style.backgroundColor = 'black';
+        if (drawstate) {
+            item.style.backgroundColor = 'black';
+        }
     });
 });
