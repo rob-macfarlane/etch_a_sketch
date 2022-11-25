fbd91a0b0995fd3893dbcbeaf998b4deac60fb9b
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-24 20:22:08 -0800

    Creates grid of h's where sketch should happen

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,19 @@
 
 const sketchBox = document.querySelector('.sketch-box');
 
-console.log(sketchBox)
+numberOfBoxes = 81;
 
-const div = document.createElement('div');
-div.classList.add('sketch-block');
-div.textContent = 'hello world!';
-sketchBox.appendChild(div);
+boxSideLength = 400 / Math.sqrt(numberOfBoxes);
+lengthString = boxSideLength + 'px'
+console.log(lengthString)
+
+
+for (let i = 0; i < numberOfBoxes; i++){
+    const sketchElement = document.createElement('div');
+    sketchElement.classList.add('sketch-element');
+    sketchElement.textContent = 'h';
+    sketchElement.style.width = lengthString
+    sketchElement.style.height = lengthString
+    sketchBox.appendChild(sketchElement);
+
+}
