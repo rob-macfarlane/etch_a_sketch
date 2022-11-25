0df68a70e0769e93da44f19db9445a3f299d5f97
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-24 22:39:21 -0800

    Removes commented old commented out code

--- js/javascript.js
+++ js/javascript.js
@@ -11,7 +11,6 @@ console.log(lengthString)
 for (let i = 0; i < numberOfBoxes; i++){
     const sketchElement = document.createElement('div');
     sketchElement.classList.add('sketch-element');
-    // sketchElement.textContent = 'h';
     sketchElement.style.width = lengthString
     sketchElement.style.height = lengthString
     sketchBox.appendChild(sketchElement);
@@ -19,10 +18,7 @@ for (let i = 0; i < numberOfBoxes; i++){
 
 var drawState = false;
 window.addEventListener("mousedown", event => {
-    // //this prevent default line could become troubling if we add a drag function later
-    // // however it is used to eliminate erroneous sketching related to sketchbox mouseenter
-    // // comment
-    // event.preventDefault();
+
     if(event.button===0){
         drawState = true;
     }
@@ -33,23 +29,11 @@ window.addEventListener("mouseup", event => {
     }
 });
 
-
-// // purpose of sketchbox mouse enter event listener
-// // It was found that you can get stuck in a drag scenario outside of the drawing square
-// // so if that happens this changes the draw state back to false when the mouse enters the sketch area
-// // however it doesn't happen quite fast enough so maybe one square will get filled
-// sketchBox.addEventListener('mouseenter', event => {
-//     drawState = false;
-// });
-
 document.querySelectorAll(':not(.sketch-box .sketch-element').forEach(item => {
     item.addEventListener('mouseenter', event => {
         drawState = false;
     });
 });
-
-
-
 
 document.querySelectorAll('.sketch-element').forEach(item => {
     item.addEventListener('mousedown', e => {
