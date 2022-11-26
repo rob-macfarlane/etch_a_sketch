5b4e9c0da1dcc6520abb402b6b62c0e507d49a5d
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-25 19:57:14 -0800

    Center the grid size text

--- js/javascript.js
+++ js/javascript.js
@@ -81,12 +81,11 @@ function resetDrawSpace() {
 };
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var output = document.getElementById("grid-size");
 output.innerHTML = slider.value + ' x ' + slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-
     output.innerHTML = this.value + ' x ' + this.value;
 }
 
