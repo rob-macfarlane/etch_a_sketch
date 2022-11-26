3260d052ab4b8f1c599ed0c469eb8ed77699095d
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-25 17:50:25 -0800

    Slider added to page but not working, reset and sketch working

--- js/javascript.js
+++ js/javascript.js
@@ -65,3 +65,12 @@ function resetDrawSpace() {
 };
 
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value + ' x ' + slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    
+    output.innerHTML = this.value + ' x ' + this.value;
+}
